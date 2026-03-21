ab5d3f8a681fe14af880b5bdfdf0275ce3401bff
Vinicius <inivsouza@gmail.com>
2026-03-20 23:36:27 -0300

    mensagem

--- code/game.py
+++ code/game.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import pygame
+from menu import Menu
+
 class Game:
     def __init__(self):
-        self.window = None
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(600, 480))
 
-    def run(self, ):
-        pass
+    def run(self):
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+ 
+ 
+ 
+       # print("Game is running")
+       # print("Game loop is starting")        
+        
+            #for event in pygame.event.get():
+                #if event.type == pygame.QUIT:
+                    #pygame.quit()
+                    #quit()
--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import pygame
+from code.game import game
 
-pygame.init()
-screen = pygame.display.set_mode(size=(600, 480))
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
+game = game()
+game.run()
