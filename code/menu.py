cc017c137d096c7e430aab38b4a8fbe150372c4c
Vinicius <inivsouza@gmail.com>
2026-03-21 00:27:17 -0300

    gio

--- code/menu.py
+++ code/menu.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame.image
 
 class Menu:
     def __init__(self, window):
         self.window = window
+        self.surt = pygame.image.load("asset/gio.png")
+        self.rect = self.surt.get_rect(left=0, top=0)
 
     def run(self, ):
+        self.window.blit(self.surt, self.rect)
+        pygame.display.flip()
+
         pass
